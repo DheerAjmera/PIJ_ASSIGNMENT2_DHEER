c31f278a155cd34e42a68da7b0864a0efa9b8ee6
Dheer <dheerajmera@gmail.com>
2025-02-03 09:36:28 +0530

    created for loop to sort even and odd numbers

---
+++ ArrayLists.java
@@ -0,0 +1,25 @@
+// ArrayLists.java
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void separateEvenOdd(int[] numbers) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        
+    }
+
+}
+
+
+
+
+
